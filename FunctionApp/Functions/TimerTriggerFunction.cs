ec9e3dfc3bfdb69b089b23ee7f3b3c9b8e9c7310
Xiyue Yu <xiyueyu@microsoft.com>
2020-07-11 17:33:33 +0800

    Add project files.

---
+++ FunctionApp/Functions/TimerTriggerFunction.cs
@@ -0,0 +1,19 @@
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace FunctionApp
+{
+    public static class TimerTriggerFunction
+    {
+        [FunctionName("TimerTriggerFunction")]
+        public static void Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log)
+        {
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+
+
+
+        }
+    }
+}
